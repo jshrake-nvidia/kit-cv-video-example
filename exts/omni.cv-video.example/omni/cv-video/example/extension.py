b6b850cda365944e8e1a764723562919ad17cc19
Justin Shrake <107588944+jshrake-nvidia@users.noreply.github.com>
2023-03-13 09:19:26 -0700

    Perform texture uploads in a separate thread (#1)
    - Creates a thread per stream. Wont scale to large numbers of streams.
    - Each thread handles the OpenCV VideoCapture read, color space conversion, and dynamic texture upload.
    - Switch to using DynamicTextureProvider::set_data_array

--- exts/omni.cv-video.example/omni/cv-video/example/extension.py
+++ exts/omni.cv-video.example/omni/cv-video/example/extension.py
@@ -1,40 +1,49 @@
-'''
+"""
 Omniverse Kit example extension that demonstrates how to stream video (such as RTSP) to a dynamic texture using [OpenCV VideoCapture](https://docs.opencv.org/3.4/dd/d43/tutorial_py_video_display.html) 
 and [omni.ui.DynamicTextureProvider](https://docs.omniverse.nvidia.com/kit/docs/omni.ui/latest/omni.ui/omni.ui.ByteImageProvider.html#byteimageprovider).
 
 TODO:
-- [ ] Investigate how to perform the color space conversion and texture updates in a separate thread
-- [ ] Investigate how to avoid the color space conversion and instead use the native format
-'''
-import omni.ext
-import omni.ui
-import omni.kit.app
-import cv2 as cv
-import numpy as np
-import carb
-import carb.profiler
+- [x] Investigate how to perform the color space conversion and texture updates in a separate thread
+    - This isn't improving performance like I might expect. After profiling, it appears we are still bottlenecked by a usd context lock
+- [ ] Investigate how to avoid the color space conversion and instead use the native format of the frame provided by OpenCV
+"""
+import asyncio
+import threading
 import time
-from pxr import Kind, Sdf, Usd, UsdGeom, UsdShade
 from typing import List
 
-DEFAULT_STREAM_URI = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
+import carb
+import carb.profiler
+import cv2 as cv
+import numpy as np
+import omni.ext
+import omni.kit.app
+import omni.ui
+import warp as wp
+from pxr import Kind, Sdf, Usd, UsdGeom, UsdShade
 
-def create_textured_plane_prim(stage: Usd.Stage, prim_path: str, texture_name: str, width: float, height: float) -> Usd.Prim:
-    '''
+DEFAULT_STREAM_URI = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
+#DEFAULT_STREAM_URI = "C:/Users/jshrake/Downloads/1080p.mp4"
+
+
+def create_textured_plane_prim(
+    stage: Usd.Stage, prim_path: str, texture_name: str, width: float, height: float
+) -> Usd.Prim:
+    """
     Creates a plane prim and an OmniPBR material with a dynamic texture for the albedo map
-    '''
+    """
     hw = width / 2
     hh = height / 2
     # This code is mostly copy pasted from https://graphics.pixar.com/usd/release/tut_simple_shading.html
     billboard: UsdGeom.Mesh = UsdGeom.Mesh.Define(stage, f"{prim_path}/Mesh")
     billboard.CreatePointsAttr([(-hw, -hh, 0), (hw, -hh, 0), (hw, hh, 0), (-hw, hh, 0)])
     billboard.CreateFaceVertexCountsAttr([4])
-    billboard.CreateFaceVertexIndicesAttr([0,1,2,3])
+    billboard.CreateFaceVertexIndicesAttr([0, 1, 2, 3])
     billboard.CreateExtentAttr([(-430, -145, 0), (430, 145, 0)])
-    texCoords = UsdGeom.PrimvarsAPI(billboard).CreatePrimvar("st",
-                                        Sdf.ValueTypeNames.TexCoord2fArray,
-                                        UsdGeom.Tokens.varying)
-    texCoords.Set([(0, 0), (1, 0), (1,1), (0, 1)])
+    texCoords = UsdGeom.PrimvarsAPI(billboard).CreatePrimvar(
+        "st", Sdf.ValueTypeNames.TexCoord2fArray, UsdGeom.Tokens.varying
+    )
+    texCoords.Set([(0, 0), (1, 0), (1, 1), (0, 1)])
 
     material_path = f"{prim_path}/Material"
     material: UsdShade.Material = UsdShade.Material.Define(stage, material_path)
@@ -48,18 +57,21 @@ def create_textured_plane_prim(stage: Usd.Stage, prim_path: str, texture_name: s
     UsdShade.MaterialBindingAPI(billboard).Bind(material)
     return billboard
 
-class OpenCvVideoStream():
-    '''
+
+class OpenCvVideoStream:
+    """
     A small abstraction around OpenCV VideoCapture and omni.ui.DynamicTextureProvider,
     making a one-to-one mapping between the two
     Resources:
     - https://docs.opencv.org/3.4/d8/dfe/classcv_1_1VideoCapture.html
     - https://docs.opencv.org/3.4/dd/d43/tutorial_py_video_display.html
     - https://docs.omniverse.nvidia.com/kit/docs/omni.ui/latest/omni.ui/omni.ui.ByteImageProvider.html#omni.ui.ByteImageProvider.set_bytes_data_from_gpu
-    '''
+    """
+
     def __init__(self, name: str, stream_uri: str):
         self.name = name
         self.uri = stream_uri
+        self.texture_array = None
         try:
             # Attempt to treat the uri as an int
             # https://docs.opencv.org/3.4/d8/dfe/classcv_1_1VideoCapture.html#a5d5f5dacb77bbebdcbfb341e3d4355c1
@@ -74,50 +86,64 @@ class OpenCvVideoStream():
         self._dynamic_texture = omni.ui.DynamicTextureProvider(name)
         self._last_read = time.time()
         self.is_ok = self._video_capture.isOpened()
-        # If this FPS is 0, set it to something sensible 
+        # If this FPS is 0, set it to something sensible
         if self.fps == 0:
             self.fps = 24
-    
+
     @carb.profiler.profile
-    def update(self):
+    def update_texture(self):
         # Rate limit frame reads to the underlying FPS of the capture stream
         now = time.time()
-        time_delta =  now - self._last_read
-        if (time_delta < 1.0/self.fps):
+        time_delta = now - self._last_read
+        if time_delta < 1.0 / self.fps:
             return
         self._last_read = now
 
         # Read the frame
+        carb.profiler.begin(0, "read")
         ret, frame = self._video_capture.read()
+        carb.profiler.end(0)
+        # The video may be at the end, loop by setting the frame position back to 0
         if not ret:
+            self._video_capture.set(cv.CAP_PROP_POS_FRAMES, 0)
+            self._last_read = time.time()
             return
 
-        # By default, OpenCV converts the frame to BGR 
+        # By default, OpenCV converts the frame to BGR
         # We need to convert the frame to a texture format suitable for RTX
         # In this case, we convert to BGRA, but the full list of texture formats can be found at
         # # kit\source\extensions\omni.gpu_foundation\bindings\python\omni.gpu_foundation_factory\GpuFoundationFactoryBindingsPython.cpp
         frame: np.ndarray
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2BGRA)
+
+        carb.profiler.begin(0, "color space conversion")
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGBA)
+        carb.profiler.end(0)
         height, width, channels = frame.shape
-        # See https://docs.omniverse.nvidia.com/kit/docs/omni.ui/latest/omni.ui/omni.ui.ByteImageProvider.html#omni.ui.ByteImageProvider.set_bytes_data_from_gpu
-        self._dynamic_texture.set_bytes_data(frame.flatten().tolist(), [width, height], omni.ui.TextureFormat.BGRA8_UNORM)
+
+        carb.profiler.begin(0, "set_bytes_data")
+        self._dynamic_texture.set_data_array(frame, [width, height, channels])
+        carb.profiler.end(0)
 
 class OmniRtspExample(omni.ext.IExt):
     def on_startup(self, ext_id):
-        stream = omni.kit.app.get_app().get_update_event_stream()
-        self._sub = stream.create_subscription_to_pop(self._on_update, name="update")
+        # stream = omni.kit.app.get_app().get_update_event_stream()
+        # self._sub = stream.create_subscription_to_pop(self._update_streams, name="update")
         self._streams: List[OpenCvVideoStream] = []
+        self._stream_threads: List[threading.Thread] = []
         self._stream_uri_model = omni.ui.SimpleStringModel(DEFAULT_STREAM_URI)
         self._window = omni.ui.Window("OpenCV Video Streaming Example", width=800, height=200)
         with self._window.frame:
             with omni.ui.VStack():
                 omni.ui.StringField(model=self._stream_uri_model)
                 omni.ui.Button("Create", clicked_fn=self._on_click_create)
-    
+
     @carb.profiler.profile
-    def _on_update(self, e):
-        for stream in self._streams:
-            stream.update()
+    def _update_stream(self, i):
+        async def loop():
+            while self._running:
+                await asyncio.sleep(0.001)
+                self._streams[i].update_texture()
+        asyncio.run(loop())
 
     def _on_click_create(self):
         name = f"Video{len(self._streams)}"
@@ -138,13 +164,25 @@ class OmniRtspExample(omni.ext.IExt):
             carb.log_error(f"Error opening stream: {stream_uri}")
             return
         self._streams.append(video_stream)
+        carb.log_info(f"Creating video steam {stream_uri} {video_stream.width}x{video_stream.height}")
         # Create the mesh + material + shader
         model_root = UsdGeom.Xform.Define(stage, prim_path)
         Usd.ModelAPI(model_root).SetKind(Kind.Tokens.component)
         create_textured_plane_prim(stage, prim_path, image_name, video_stream.width, video_stream.height)
         # Clear the string model
-        #self._stream_uri_model.set_value("")
+        # self._stream_uri_model.set_value("")
+        # Create the thread to pump the video stream
+        self._running = True
+        i = len(self._streams) - 1
+        thread = threading.Thread(target=self._update_stream, args=(i, ))
+        thread.daemon = True
+        thread.start()
+        self._stream_threads.append(thread)
 
     def on_shutdown(self):
-        self._sub.unsubscribe()
+        # self._sub.unsubscribe()
+        self._running = False
+        for thread in self._stream_threads:
+            thread.join()
+        self._stream_threads = []
         self._streams = []
